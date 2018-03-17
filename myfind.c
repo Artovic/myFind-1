5347dc46e5046dd41c2e254062d7c6ab7e8277bd
dom-mages <dominic.mages@gmail.com>
2018-03-17 03:02:20 +0100

    parse_params and free_params

--- myfind.c
+++ myfind.c
@@ -13,61 +13,111 @@
 // @todo Add required fucntionality.
 //
 
+#include <dirent.h>
 #include <errno.h>
-#include <error.h>
+#include <fnmatch.h>
+#include <grp.h>
+#include <libgen.h>
+#include <limits.h>
+#include <locale.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
+#include <string.h>
 #include <sys/stat.h>
+#include <time.h>
 #include <unistd.h>
-#include <string.h> // strerror
+
+// linked list that contains all params
+typedef struct s_params
+{
+    char *location;
+    int print;
+    int ls;
+    int nouser;
+    char type;
+    char *user;
+    unsigned int userid;
+    char *path;
+    char *name;
+    struct s_params *next;
+} t_params;
+
+// global for program name - think about a workaround for this
+char *programName = "";
 
 // Prototypes
-void do_file(const char *fp_path, const struct stat *atrr);
-void do_dir(const char *dp_path, const struct stat *sb);
+int parse_params(int argc, char *argv[], t_params *params);
+int clear_params(t_params *params);
+void do_file(const char *fp_path, struct stat *atrr);
+void do_dir(const char *dp_path, struct stat *sb);
+int do_user(unsigned int userid, struct stat *attr);
+int do_path(char *path, char *pattern);
+int do_name(char *path, char *pattern);
+int do_type(char type, struct stat *attr);
+int do_print(const char *fp_path);
+int do_ls(char *path, struct stat *attr);
+// int do_nouser(const stat *attr);
 
 int main(int argc, char *argv[])
 {
+    t_params *params;
+    programName = argv[0];
     // prevent warnings regarding unused params
     argc = argc;
     // argv = argv;
 
+    //
+    params = calloc(1, sizeof(*params));
+    if (!params)
+    {
+        fprintf(stderr, "%s: calloc(): %s\n", programName, strerror(errno));
+        return 1;
+    }
+
+    if (parse_params(argc, argv, params) != 0)
+    {
+        // clear_params(params);
+        return 1;
+    }
+
     char *location = argv[1];
     struct stat *attr = NULL;
 
-//TODO: lstat is ueberflussig --> da in do_file passiert
-//TODO: in main only do_file, rest handled in those functions
-        do_file(location, attr);
-//TODO: argv[0] anstatt hardcoded filename
+    //TODO: lstat is ueberflussig --> da in do_file passiert
+    //TODO: in main only do_file, rest handled in those functions
+    do_file(location, attr);
 
+    // clear_params(params);
     return 0;
 }
 
 // TODO: aParams - to mangage all params including filename and targetdirectory
-// do_file - simple in the beginning, will grow enormeous
-void do_file(const char *fp_path, const struct stat *attr)
+// @ralph, david: Hab das zurückgeändert in struct stat *attr, weil ich nicht verstehe, wie das mit const funktionieren soll
+// ls stat verweigert eine verarbeitung mit const... ka was der galla da dann will....
+void do_file(const char *fp_path, struct stat *attr)
 {
-    errno = 0;
-    if(lstat(fp_path, attr) == 0) {
-	
-    }
-//TODO: erster entry kann file oder directory sein und muss vorhanden sein
-//TODO: lstat hier, wenn nicht --> exit code
 
-//TODO: if (S_ISDIR(sb.st_mode)) check in do_file. if true, call do_dir, if falsy, countinue do_file 
-    attr = attr; //prevent unused params, need for later implementation
+    errno = 0;
+    if (lstat(fp_path, attr) == 0)
+    {
+    }
+    //TODO: erster entry kann file oder directory sein und muss vorhanden sein
+    //TODO: lstat hier, wenn nicht --> exit code
+
+    //TODO: if (S_ISDIR(sb.st_mode)) check in do_file. if true, call do_dir, if falsy, countinue do_file
     //if(lstat(fp_path, attr) != 0) {
     //    fprintf(stderr, "here is my awesome argv[0]: lstat(%s): %s\n", fullpath, strerror(errno));
     //}
     //if(S_ISDIR(attr->st_mode)) {
-//	do_dir(fp_path, 
-  //  }
+    //	do_dir(fp_path,
+    //  }
     printf("%s\n", fp_path);
-//    return 0;
+    //    return 0;
 }
 
 // do_dir - main logic for intermediate showcase
-void do_dir(const char *dpath, const struct stat *sb)
+void do_dir(const char *dpath, struct stat *sb)
 {
     DIR *dir;
     struct dirent *entry;
@@ -79,7 +129,7 @@ void do_dir(const char *dpath, const struct stat *sb)
 
     if (!dir)
     {
-        fprintf(stderr, "myfind: opendir(%s): %s\n", dpath, strerror(errno));
+        fprintf(stderr, "%s: opendir(%s): %s\n", programName, dpath, strerror(errno));
         return;
     }
 
@@ -104,18 +154,213 @@ void do_dir(const char *dpath, const struct stat *sb)
         /* concat the path with the entry name */
         if (snprintf(fullpath, pathlength, "%s%s%s", dpath, slash, entry->d_name) < 0)
         {
-            fprintf(stderr, "myfind: snprintf(): %s\n", strerror(errno));
+            fprintf(stderr, "%s: snprintf(): %s\n", programName, strerror(errno));
             break;
         }
         do_file(fullpath, sb);
     }
 
-    if (errno != 0) {
-	fprintf(stderr, "my fucking awesome argv[0] replacement: readdir(%s): %s\n", dpath, strerror(errno));
+    if (errno != 0)
+    {
+        fprintf(stderr, "%s: readdir(%s): %s\n", programName, dpath, strerror(errno));
     }
     if (closedir(dir) != 0)
     {
-        fprintf(stderr, "myfind: closedir(%s): %s\n", dpath, strerror(errno));
+        fprintf(stderr, "%s: closedir(%s): %s\n", programName, dpath, strerror(errno));
         return;
     }
+}
+
+int parse_params(int argc, char *argv[], t_params *params)
+{
+    struct passwd *pwd;
+    int i; /* used outside of the loop */
+
+    int status = 0;
+    int expression = 0;
+
+    /* params can start from argv[1] */
+    for (i = 1; i < argc; i++, params = params->next)
+    {
+
+        /* allocate memory for the next run */
+        params->next = calloc(1, sizeof(*params));
+
+        if (!params->next)
+        {
+            fprintf(stderr, "%s: calloc(): %s\n", programName, strerror(errno));
+            return 1;
+        }
+
+        /* parameters consisting of a single part */
+        if (strcmp(argv[i], "-print") == 0)
+        {
+            params->print = 1;
+            expression = 1;
+            continue;
+        }
+        if (strcmp(argv[i], "-ls") == 0)
+        {
+            params->ls = 1;
+            expression = 1;
+            continue;
+        }
+        if (strcmp(argv[i], "-nouser") == 0)
+        {
+            params->nouser = 1;
+            expression = 1;
+            continue;
+        }
+
+        /* parameters expecting a non-empty second part */
+        if (strcmp(argv[i], "-user") == 0)
+        {
+            if (argv[++i])
+            {
+                params->user = argv[i];
+                /* check if the user exists */
+                if ((pwd = getpwnam(params->user)))
+                {
+                    params->userid = pwd->pw_uid;
+                    expression = 1;
+                    continue;
+                }
+                /* otherwise, if the input is a number, use that */
+                if (sscanf(params->user, "%u", &params->userid))
+                {
+                    expression = 1;
+                    continue;
+                }
+                status = 4;
+                break; /* the user is not found and not a number */
+            }
+            else
+            {
+                status = 2;
+                break; /* the second part is missing */
+            }
+        }
+        if (strcmp(argv[i], "-name") == 0)
+        {
+            if (argv[++i])
+            {
+                params->name = argv[i];
+                expression = 1;
+                continue;
+            }
+            else
+            {
+                status = 2;
+                break; /* the second part is missing */
+            }
+        }
+        if (strcmp(argv[i], "-path") == 0)
+        {
+            if (argv[++i])
+            {
+                params->path = argv[i];
+                expression = 1;
+                continue;
+            }
+            else
+            {
+                status = 2;
+                break; /* the second part is missing */
+            }
+        }
+
+        /* a parameter expecting a restricted second part */
+        if (strcmp(argv[i], "-type") == 0)
+        {
+            if (argv[++i])
+            {
+                if ((strcmp(argv[i], "b") == 0) || (strcmp(argv[i], "c") == 0) ||
+                    (strcmp(argv[i], "d") == 0) || (strcmp(argv[i], "p") == 0) ||
+                    (strcmp(argv[i], "f") == 0) || (strcmp(argv[i], "l") == 0) ||
+                    (strcmp(argv[i], "s") == 0))
+                {
+                    params->type = argv[i][0];
+                    expression = 1;
+                    continue;
+                }
+                else
+                {
+                    status = 3;
+                    break; /* the second part is unknown */
+                }
+            }
+            else
+            {
+                status = 2;
+                break; /* the second part is missing */
+            }
+        }
+
+        /*
+     * there was no match;
+     * if the parameter starts with '-', return an error,
+     * else if there were no previous matches (expressions), save it as a location
+     */
+        if (argv[i][0] == '-')
+        {
+            status = 1;
+            break;
+        }
+        else
+        {
+            if (expression == 0)
+            {
+                params->location = argv[i];
+                continue;
+            }
+            else
+            {
+                status = 5;
+                break;
+            }
+        }
+    }
+
+    /* error handling */
+    if (status == 1)
+    {
+        fprintf(stderr, "%s: unknown predicate: `%s'\n", programName, argv[i]);
+        return 1;
+    }
+    if (status == 2)
+    {
+        fprintf(stderr, "%s: missing argument to `%s'\n", programName, argv[i - 1]);
+        return 1;
+    }
+    if (status == 3)
+    {
+        fprintf(stderr, "%s: unknown argument to %s: %s\n", programName, argv[i - 1], argv[i]);
+        return 1;
+    }
+    if (status == 4)
+    {
+        fprintf(stderr, "%s: `%s' is not the name of a known user\n", programName, argv[i]);
+        return 1;
+    }
+    if (status == 5)
+    {
+        fprintf(stderr, "%s: paths must precede expression: %s\n", programName, argv[i]);
+        fprintf(stderr, "Usage: %s <location> [ <aktion> ]\n", programName);
+        return 1;
+    }
+
+    return 0;
+}
+
+int do_free_params(t_params *params)
+{
+
+    while (params)
+    {
+        t_params *next = params->next;
+        free(params);
+        params = next;
+    }
+
+    return 1;
 }

dd41c9d8eaedf8120a1eb9cb0348fbbcb467646e
dom-mages <dominic.mages@gmail.com>
2018-03-26 23:49:44 +0200

    do_ls, do_type, do_user

--- myfind.c
+++ myfind.c
@@ -52,11 +52,11 @@ int free_params(t_params *params);
 void do_file(const char *fp_path, t_params *params);
 void do_dir(const char *dp_path, t_params *params);
 int do_user(unsigned int userid, struct stat *attr);
-int do_path(char *path, char *pattern);
-int do_name(char *path, char *pattern);
 int do_type(char type, struct stat *attr);
-int do_print(const char *fp_path);
 int do_ls(char *path, struct stat *attr);
+int do_name(char *path, char *pattern);
+int do_print(const char *fp_path);
+int do_path(char *path, char *pattern);
 // int do_nouser(const stat *attr);
 
 int main(int argc, char *argv[])
@@ -91,29 +91,33 @@ int main(int argc, char *argv[])
 
 // TODO: aParams - to mangage all params including filename and targetdirectory
 // @ralph, david: Hab das zurückgeändert in struct stat *attr, weil ich nicht verstehe, wie das mit const funktionieren soll
-// ls stat verweigert eine verarbeitung mit const... ka was der galla da dann will....
 void do_file(const char *fp_path, t_params *params)
 {
+    errno = 0;
     struct stat attr;
 
-    errno = 0;
+    int wasPrinted = 0;
+    wasPrinted = wasPrinted;
+
+    do
+    {
+
+        params = params->next;
+    } while (params);
+
+    //TODO: if (S_ISDIR(sb.st_mode)) check in do_file. if true, call do_dir, if falsy, countinue do_file
     if (lstat(fp_path, &attr) == 0)
     {
-        // printf("%s\n", fp_path);
+        printf("%s\n", fp_path);
         if (S_ISDIR(attr.st_mode))
         {
             do_dir(fp_path, params);
         }
     }
-    //TODO: erster entry kann file oder directory sein und muss vorhanden sein
-    //TODO: lstat hier, wenn nicht --> exit code
-
-    //TODO: if (S_ISDIR(sb.st_mode)) check in do_file. if true, call do_dir, if falsy, countinue do_file
     else
     {
         fprintf(stderr, "%s: lstat(%s): %s\n", programName, fp_path, strerror(errno));
-        }
-    //    return 0;
+    }
 }
 
 // do_dir - main logic for intermediate showcase
@@ -174,16 +178,15 @@ void do_dir(const char *dpath, t_params *params)
 int parse_params(int argc, char *argv[], t_params *params)
 {
     struct passwd *pwd;
-    int i; /* used outside of the loop */
+    int i; // used outside of the loop
 
     int status = 0;
-    int expression = 0;
 
-    /* params can start from argv[1] */
-    for (i = 1; i < argc; i++, params = params->next)
+    // params can start from argv[2] because 0 is progname and 1 is path
+    for (i = 2; i < argc; i++, params = params->next)
     {
 
-        /* allocate memory for the next run */
+        // allocate memory for the next run
         params->next = calloc(1, sizeof(*params));
 
         if (!params->next)
@@ -192,52 +195,47 @@ int parse_params(int argc, char *argv[], t_params *params)
             return 1;
         }
 
-        /* parameters consisting of a single part */
+        // single part params
         if (strcmp(argv[i], "-print") == 0)
         {
             params->print = 1;
-            expression = 1;
             continue;
         }
         if (strcmp(argv[i], "-ls") == 0)
         {
             params->ls = 1;
-            expression = 1;
             continue;
         }
         if (strcmp(argv[i], "-nouser") == 0)
         {
             params->nouser = 1;
-            expression = 1;
             continue;
         }
 
-        /* parameters expecting a non-empty second part */
+        // params that require a second part
         if (strcmp(argv[i], "-user") == 0)
         {
             if (argv[++i])
             {
                 params->user = argv[i];
-                /* check if the user exists */
+                // check if user exists
                 if ((pwd = getpwnam(params->user)))
                 {
                     params->userid = pwd->pw_uid;
-                    expression = 1;
                     continue;
                 }
-                /* otherwise, if the input is a number, use that */
+                // or if the value is a number
                 if (sscanf(params->user, "%u", &params->userid))
                 {
-                    expression = 1;
                     continue;
                 }
                 status = 4;
-                break; /* the user is not found and not a number */
+                break; // user is wether a number nor existing
             }
             else
             {
                 status = 2;
-                break; /* the second part is missing */
+                break; // value is missing
             }
         }
         if (strcmp(argv[i], "-name") == 0)
@@ -245,13 +243,12 @@ int parse_params(int argc, char *argv[], t_params *params)
             if (argv[++i])
             {
                 params->name = argv[i];
-                expression = 1;
                 continue;
             }
             else
             {
                 status = 2;
-                break; /* the second part is missing */
+                break; // value is missing
             }
         }
         if (strcmp(argv[i], "-path") == 0)
@@ -259,17 +256,16 @@ int parse_params(int argc, char *argv[], t_params *params)
             if (argv[++i])
             {
                 params->path = argv[i];
-                expression = 1;
                 continue;
             }
             else
             {
                 status = 2;
-                break; /* the second part is missing */
+                break; // value is missing
             }
         }
 
-        /* a parameter expecting a restricted second part */
+        // params that expects a second part that can only be an existing file descriptor
         if (strcmp(argv[i], "-type") == 0)
         {
             if (argv[++i])
@@ -280,27 +276,22 @@ int parse_params(int argc, char *argv[], t_params *params)
                     (strcmp(argv[i], "s") == 0))
                 {
                     params->type = argv[i][0];
-                    expression = 1;
                     continue;
                 }
                 else
                 {
                     status = 3;
-                    break; /* the second part is unknown */
+                    break; // value is not a valid file descriptor
                 }
             }
             else
             {
                 status = 2;
-                break; /* the second part is missing */
+                break; // value is missing
             }
         }
 
-        /*
-     * there was no match;
-     * if the parameter starts with '-', return an error,
-     * else if there were no previous matches (expressions), save it as a location
-     */
+        // handler for params that do not exist
         if (argv[i][0] == '-')
         {
             status = 1;
@@ -308,16 +299,8 @@ int parse_params(int argc, char *argv[], t_params *params)
         }
         else
         {
-            if (expression == 0)
-            {
-                params->location = argv[i];
-                continue;
-            }
-            else
-            {
-                status = 5;
-                break;
-            }
+            status = 5;
+            break;
         }
     }
 
@@ -345,7 +328,7 @@ int parse_params(int argc, char *argv[], t_params *params)
     if (status == 5)
     {
         fprintf(stderr, "%s: paths must precede expression: %s\n", programName, argv[i]);
-        fprintf(stderr, "Usage: %s <location> [ <aktion> ]\n", programName);
+        fprintf(stderr, "Usage: %s <file or directory> [ <action> ]\n", programName);
         return 1;
     }
 
@@ -363,4 +346,234 @@ int free_params(t_params *params)
     }
 
     return 1;
+}
+
+int do_user(unsigned int userid, struct stat *attr)
+{
+    if (userid == (unsigned int)attr->st_uid)
+        return 0;
+    else
+        return 1;
+}
+
+int do_type(char type, struct stat *attr)
+{
+    int checkvalue;
+    switch (type)
+    {
+    case 'd':
+        checkvalue = S_ISDIR(attr->st_mode);
+        break;
+    case 'b':
+        checkvalue = S_ISBLK(attr->st_mode);
+        break;
+    case 'c':
+        checkvalue = S_ISCHR(attr->st_mode);
+        break;
+    case 'p':
+        checkvalue = S_ISFIFO(attr->st_mode);
+        break;
+    case 'f':
+        checkvalue = S_ISREG(attr->st_mode);
+        break;
+    case 'l':
+        checkvalue = S_ISLNK(attr->st_mode);
+        break;
+    case 's':
+        checkvalue = S_ISSOCK(attr->st_mode);
+        break;
+    default:
+        checkvalue = 0;
+        break;
+    }
+
+    if (checkvalue != 0)
+        return 0;
+    else
+        return 1;
+}
+
+int do_ls(char *path, struct stat *attr)
+{
+    //-rwxrwxrwx 1 root root 1234 March 17 10:00 filename.exention
+    int i = 0;
+    char typ = '0';
+    char *user;
+    char *group;
+    struct passwd *pwd;
+    struct group *grp;
+    char *stime = "";
+    time_t t = time(NULL);
+    struct tm *deadline_time = localtime(&t);
+    if (deadline_time == NULL)
+        return 1;
+    deadline_time->tm_year = deadline_time->tm_year + 1900;
+    struct tm *modified_time = localtime(&attr->st_mtime);
+    if (modified_time == NULL)
+        return 1;
+    modified_time->tm_year = modified_time->tm_year + 1900;
+    int flag = 1;
+    char *filename;
+    char *symlink;
+    char *pfeil;
+
+    //user and group name
+    grp = getgrgid((unsigned int)attr->st_gid);
+    if (grp == NULL)
+        return 1;
+    group = grp->gr_name;
+
+    pwd = getpwuid((unsigned int)attr->st_uid);
+    if (pwd == NULL)
+        user = group;
+    else
+        user = pwd->pw_name;
+
+    //actual date - 6 Months
+    deadline_time->tm_mon = deadline_time->tm_mon - 6;
+    if (deadline_time->tm_mon < 0)
+    {
+        deadline_time->tm_mon = deadline_time->tm_mon + 12;
+        deadline_time->tm_year = deadline_time->tm_year - 1;
+    }
+    if ((deadline_time->tm_mon == 3 || deadline_time->tm_mon == 5 || deadline_time->tm_mon == 8 || deadline_time->tm_mon == 10) && deadline_time->tm_mday == 31) //check if 31st in April, June, September, November
+    {
+        deadline_time->tm_mday = 30;
+    }
+    if (deadline_time->tm_mon == 1 && deadline_time->tm_mday > 28) //February (28 or 29 days)
+    {
+        if (deadline_time->tm_year % 100 == 0)
+        {
+            if (deadline_time->tm_year % 400 == 0 && deadline_time->tm_mday > 29)
+            { //leapyear
+                deadline_time->tm_mday = 29;
+            }
+            else
+            { //no leapyear
+                deadline_time->tm_mday = 28;
+            }
+        }
+        else
+        {
+            if (deadline_time->tm_year % 4 == 0 && deadline_time->tm_mday > 29)
+            { //leapyear
+                deadline_time->tm_mday = 29;
+            }
+            else
+            { //no leapyear
+                deadline_time->tm_mday = 28;
+            }
+        }
+    }
+
+    //check if last modification is older than 6 months -> if true, display year instead of time
+    if (deadline_time->tm_year < modified_time->tm_year)
+        flag = 1; //year deadline_time < year modified_time = show time
+    else if (deadline_time->tm_year == modified_time->tm_year)
+    {
+        if (deadline_time->tm_mon < modified_time->tm_mon)
+            flag = 1; //years equal, month deadline_time < month modified_time = show time
+        else if (deadline_time->tm_mon == modified_time->tm_mon)
+        {
+            if (deadline_time->tm_mday <= modified_time->tm_mday)
+                flag = 1; // years equal, months equal, day deadline_time <= day modified_time = show time
+            else
+                flag = 0; // years equal, months equal, day deadline_time > day modified_time = show year
+        }
+        else
+            flag = 0; //years equal, month deadline_time > month modified_time = show year
+    }
+    else
+    {
+        flag = 0; //year deadline_time > year modified_time = show year
+    }
+
+    if (flag == 1)
+    {
+        if (strftime(stime, 36, "%b %d %H:%S", localtime(&attr->st_mtime)) == 0)
+            return 1;
+    }
+    else
+    {
+        if (strftime(stime, 36, "%b %d %Y", localtime(&attr->st_mtime)) == 0)
+            return 1;
+    }
+
+    //type of file
+    while (i < 7 && typ == '0')
+    {
+        switch (i)
+        {
+        case 0:
+            (S_ISDIR(attr->st_mode)) ? (typ = 'd') : (0);
+            break;
+        case 1:
+            (S_ISBLK(attr->st_mode)) ? (typ = 'b') : (0);
+            break;
+        case 2:
+            (S_ISCHR(attr->st_mode)) ? (typ = 'c') : (0);
+            break;
+        case 3:
+            (S_ISFIFO(attr->st_mode)) ? (typ = '-') : (0);
+            break;
+        case 4:
+            (S_ISREG(attr->st_mode)) ? (typ = 'f') : (0);
+            break;
+        case 5:
+            (S_ISLNK(attr->st_mode)) ? (typ = 'l') : (0);
+            break;
+        case 6:
+            (S_ISSOCK(attr->st_mode)) ? (typ = 's') : (0);
+            break;
+        }
+    }
+    if (typ == 0)
+        return 1;
+
+    //filename
+    if (typ == 'd')
+        filename = dirname(path);
+    else
+        filename = basename(path);
+
+    //symbolic link
+    if (typ == 'l')
+    {
+        errno = 0;
+        // readlink(filename, symlink, attr->st_size + 1);
+        if (errno != 0)
+        {
+            return 1;
+        }
+        symlink[attr->st_size] = '\0';
+        pfeil = " -> ";
+    }
+    else
+    {
+        symlink = "";
+        pfeil = "";
+    }
+
+    //print
+    printf("%c%c%c%c%c%c%c%c%c%c %d %s %s %ld %s %s %s %s",
+           typ,
+           (S_IRUSR & attr->st_mode) ? ('r') : ('-'),
+           (S_IWUSR & attr->st_mode) ? ('w') : ('-'),
+           (S_ISUID & attr->st_mode) ? ((S_IXUSR & attr->st_mode) ? ('s') : ('S')) : ((S_IXUSR & attr->st_mode) ? ('x') : ('-')),
+           (S_IRGRP & attr->st_mode) ? ('r') : ('-'),
+           (S_IWGRP & attr->st_mode) ? ('w') : ('-'),
+           (S_ISGID & attr->st_mode) ? ((S_IXGRP & attr->st_mode) ? ('s') : ('S')) : ((S_IXGRP & attr->st_mode) ? ('x') : ('-')),
+           (S_IROTH & attr->st_mode) ? ('r') : ('-'),
+           (S_IWOTH & attr->st_mode) ? ('w') : ('-'),
+           (S_ISVTX & attr->st_mode) ? ((S_IXOTH & attr->st_mode) ? ('t') : ('T')) : ((S_IXOTH & attr->st_mode) ? ('x') : ('-')),
+           (unsigned int)attr->st_nlink,
+           user,
+           group,
+           attr->st_blocks,
+           stime,
+           filename,
+           pfeil,
+           symlink);
+
+    return 0;
 }
